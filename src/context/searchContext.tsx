31b0992f8faff03c8afb423e3bc91c576e93e488
My Name <walker.b.lobato@gmail.com>
2022-11-09 01:37:02 -0300

    refactor: delete brachs

--- src/context/searchContext.tsx
+++ src/context/searchContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext,  useState } from 'react';
+import { createContext, useContext, useCallback, useState } from 'react';
 
 interface ISearchContextData {
     searchValue: string;
